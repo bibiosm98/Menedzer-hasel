8f5a8d9ca2066692364eb748607d6d3b2fe86c34
Norbert <bibiosm98@gmail.com>
2020-06-14 23:49:29 +0200

    Start client-server RSA for passwordStrength

--- public/javascript/password.js
+++ public/javascript/password.js
@@ -1,14 +1,31 @@
 $("#password").on('change', () => {
     const password = $("#password").val()
+    let dane = {
+        "password": password
+    }
+    console.log(dane)
     $.ajax({
-        type: "post",
-        url: 'http://localhost:3000/signup/checkStrength',
-        data: {
-            "password": password
-        },
-    }).done((res) => {
-        console.log(res);
-        $("#strength").html(res.strength)
+        // url: 'https://menedzerhasel.herokuapp.com/signup/PublicRsaKey',
+        url: 'http://localhost:3000/signup/PublicRsaKey',
+        method: 'get',
+    }).done(async (publicRsaKey) => {
+        console.log("Getting public RSA key from server")
+        console.log(publicRsaKey)
+        // let key = encryptoKey();
+        let key = await importPublicKey(publicRsaKey);
+        console.log("KEY imported = ")
+        console.log(key)
+        
+        $.ajax({
+            type: "post",
+            // url: 'https://menedzerhasel.herokuapp.com/signup/checkStrength',
+            url: 'http://localhost:3000/signup/checkStrength',
+            data: {"password": password}
+            // data: {"password": encrypto(key, dane)}
+        }).done((res) => {
+            console.log(res);
+            $("#strength").html(res.strength)
+        })
     })
 })
 
@@ -26,4 +43,131 @@ function check(){
         $('#password-error').html("Passwords not equals");
         $('#btn-submit').prop('disabled', true);
     }
+}
+
+
+// -----BEGIN RSA PUBLIC KEY-----
+// MIGJAoGBANHjPrZ866c2dcG5tF3Uacrw5eWMzb3EfqpxuXEjyKga+nxTBM5Q0Ica
+// 3iaSSvzqjWNgU51qga00OJlbV0cVH38X/itKJWOEyKA46PA5/GrWg1znavE6fvC6
+// mF0OaYI/FSwnHwiF/d/QcU7LVdj7pJugLOu6RhFUkGdlzzeWr65FAgMBAAE=
+// -----END RSA PUBLIC KEY-----
+
+// function encryptoKey(){
+//     window.crypto.subtle.generateKey(
+//         {
+//             name: "RSA-OAEP",
+//             modulusLength: 2048, //can be 1024, 2048, or 4096
+//             publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+//             hash: {name: "SHA-256"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+//         },
+//         false, //whether the key is extractable (i.e. can be used in exportKey)
+//         ["encrypt", "decrypt"] //must be ["encrypt", "decrypt"] or ["wrapKey", "unwrapKey"]
+//     )
+//     .then(function(key){
+//         //returns a keypair object
+//         console.log(key);
+//         console.log(key.publicKey);
+//         console.log(key.privateKey);
+//         return(key.publicKey)
+//     })
+//     .catch(function(err){
+//         console.error(err);
+//     });
+// }
+
+// function importKey(){
+//     window.crypto.subtle.importKey(
+//         "jwk", //can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+//         {   //this is an example jwk key, other key types are Uint8Array objects
+//             kty: "RSA",
+//             e: "AQAB",
+//             n: "vGO3eU16ag9zRkJ4AK8ZUZrjbtp5xWK0LyFMNT8933evJoHeczexMUzSiXaLrEFSyQZortk81zJH3y41MBO_UFDO_X0crAquNrkjZDrf9Scc5-MdxlWU2Jl7Gc4Z18AC9aNibWVmXhgvHYkEoFdLCFG-2Sq-qIyW4KFkjan05IE",
+//             alg: "RSA-OAEP-1",
+//             ext: true,
+//         },
+//         {   //these are the algorithm options
+//             name: "RSA-OAEP",
+//             hash: {name: "SHA-1"}, //can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+//         },
+//         false, //whether the key is extractable (i.e. can be used in exportKey)
+//         ["encrypt"] //"encrypt" or "wrapKey" for public key import or
+//                     //"decrypt" or "unwrapKey" for private key imports
+//     )
+//     .then(function(publicKey){
+//         //returns a publicKey (or privateKey if you are importing a private key)
+//         console.log(publicKey);
+//     })
+//     .catch(function(err){
+//         console.error(err);
+//     });
+// }
+
+// function encrypto(publicKey, data){
+
+//     window.crypto.subtle.encrypt(
+//         {
+//             name: "RSA-OAEP",
+//             //label: Uint8Array([...]) //optional
+//         },
+//         publicKey, //from generateKey or importKey above
+//         //data //ArrayBuffer of data you want to encrypt
+//         new ArrayBuffer(data)
+//     )
+//     .then(function(encrypted){
+//         //returns an ArrayBuffer containing the encrypted data
+//         console.log(new Uint8Array(encrypted));
+//     })
+//     .catch(function(err){
+//         console.error(err);
+//     });
+// }
+
+function convertPemToBinary(pem) {
+    var lines = pem.split('\n')
+    console.log("conver pem to binary")
+    var encoded = ''
+    for(var i = 0;i < lines.length;i++){
+      if (lines[i].trim().length > 0 &&
+          lines[i].indexOf('-----BEGIN RSA PRIVATE KEY-----') < 0 &&
+          lines[i].indexOf('-----BEGIN RSA PUBLIC KEY-----') < 0 &&
+          lines[i].indexOf('-----END RSA PRIVATE-----') < 0 &&
+          lines[i].indexOf('-----END RSA PUBLIC KEY-----') < 0) {
+        encoded += lines[i].trim()
+      }
+    }
+    return base64StringToArrayBuffer(encoded)
+}
+function base64StringToArrayBuffer(b64str) {
+    console.log("base64StringToArrayBuffer")
+    var byteStr = atob(b64str)
+    var bytes = new Uint8Array(byteStr.length)
+    for (var i = 0; i < byteStr.length; i++) {
+        bytes[i] = byteStr.charCodeAt(i)
+    }
+    console.log("RETURN base64StringToArrayBuffer")
+    console.log(bytes)
+    console.log(bytes.buffer)
+    return bytes.buffer
+}
+
+function importPublicKey(pemKey) {
+    return new Promise(function(resolve) {
+        var importer = crypto.subtle.importKey("spki", convertPemToBinary(pemKey), signAlgorithm, true, ["encrypt"])
+            importer.then(function(key) {
+                console.log("PUBLIC KEY = ")
+                console.log(key)
+                resolve(key)
+            }
+        )
+    })
+}
+
+var signAlgorithm = {
+    name: "RSA-PKCS1",
+    hash: {
+        name: "SHA-256"
+    },
+    modulusLength: 1024,
+    extractable: false,
+    publicExponent: new Uint8Array([1, 0, 1])
 }
--- routes/signup.js
+++ routes/signup.js
@@ -2,6 +2,19 @@ const express = require('express')
 const router = express.Router()
 const request = require('request')
 
+
+// const RSA = require('node-rsa')
+// let rsa = new RSA({b: 1024})
+
+let clientRSA = new require('node-rsa')({b: 1024})
+
+clientRSA.setOptions('pkcs8-public')
+const clienKeyRSApublic = clientRSA.exportKey('pkcs1-public-pem')
+const clientKeyRSAprivate = clientRSA.exportKey('pkcs1-pem')
+
+console.log(clienKeyRSApublic)
+console.log(clientKeyRSAprivate)
+
 router.get('/', (req, res) => {
     res.render('signup', {data: {}})
 })
@@ -47,13 +60,43 @@ router.post('/checkStrength', (req, res) => {
     console.log(req.body.password)
     request.get('https://fast-ridge-60024.herokuapp.com/api/GetPublicKey', (error, response, body) => {
         if (!error) {
-            console.log(body)
+            console.log("Encrypted with pure RSA")
+            console.log()
+            console.log()
+            console.log(req.body.password)
             // let key = new RSA(serverPublicKey)
             // key.setOptions('pkcs1_oaep')
             // let mess = key.encrypt(message, 'base64');
             // resolve(mess)
-            const RSA = require('node-rsa')
-            const rsa = new RSA(body)
+            // const keyRSApublic = rsa.exportKey('pkcs1-public-pem')
+            // const keyRSAprivate = rsa.exportKey('pkcs1-pem')
+            // console.log()
+            // console.log(keyRSApublic)
+            // console.log()
+            // console.log(keyRSAprivate)
+             rsa = new RSA(`-----BEGIN RSA PRIVATE KEY-----
+             MIICXQIBAAKBgQDZNvsSpBVZsiRWYuTZBRdEiDhb7p84suQn01NmnED8G165M/lE
+             ETHKgj4XANpnllepSchDIeCcxVwLi2jBbouI2R7f7p1rCx9AJywrZ2+Uq6TDlgu4
+             1r2sDBlMO10kBu6cI97RLS0Vuy2gCv0+tU0kBakiAfirSnUgcUm5MWnuIQIDAQAB
+             AoGAWX3eeXKi3T6Y3AKImKURTgn1dmLctKZ3e7UEYn00BOpd3137b8DJy85XOHEA
+             ircVTMj6OjZl7REe+B6qvS8HS0rpUZDv944wllRXVUJccWyEkrc7i2q7ifhsfp4r
+             QklRv8b+soP4DmE/KoRhdz7tVc+twUSqJbW9tsSuT0W7kzECQQD3Ty11L9kmJmSF
+             Go6c4Z3lNzkmQcCXir/8ED7dr47itZjBG0+0PHp7LilYieD1iSadKIMQ+np4bzaO
+             tttMP5INAkEA4NkRySPqclp2FNTjvc4kzm4UN+K/SoURx2fOZTxV7OGKFi4bU21W
+             PlL07HTUcIuUYrasXNipLMgtWVpBYVnLZQJBAMrUf13bTmeBmXOpIkn8qLkzn4WG
+             6+PeAjNDR6wdrjy134CRW79g0rtwUqyw+HWodKj0bvpf1E4uODq4MgfJI4kCQQC0
+             zznTMpOMY5xDAjMlZckJS1Hs4uUK6tkyLhK2K/+43mFplqgYvXq14YXdQ9G1szie
+             7ODMRNo+DbcR/FdJDqlVAkAHw3viKfLtL3iz14YFk1rJdlgmPOBwLoRi3ph2s+jB
+             iO+BXfwn/zktg9ZdRi31m2af5Ga4JqEOTKcd6/DiSdrJ
+             -----END RSA PRIVATE KEY-----`)
+                
+            rsa.setOptions('pkcs1')
+            let decrypted = rsa.decrypt(Buffer.from(req.body.password).toString(), 'base64')//.toString('base64')
+            console.log('Decrypted')
+            console.log()
+            console.log(decrypted)
+            console.log()
+            rsa = new RSA(body)
             rsa.setOptions('pkcs1_oaep')
             let encrypted = Buffer.from(rsa.encrypt(JSON.stringify({"password": req.body.password})), 'base64').toString('base64')
             console.log(encrypted)
@@ -69,6 +112,11 @@ router.post('/checkStrength', (req, res) => {
             })
         }
     })
+})
+
+
+router.get('/PublicRsaKey', (req, res) => {
+    res.send(clienKeyRSApublic)
 })
 module.exports = router
 
